81a0f14ba8e1d949bc2092fba232a5fc630085c5
dima <1988maksyutenko@gmail.com>
2022-09-23 15:24:52 +0300

    The client get method added.

--- client/main.go
+++ client/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello client")
+	response, responseError := http.Get("http://127.0.0.1:8989/")
+	if responseError != nil {
+		log.Panic(responseError)
+	}
+
+	data, readError := ioutil.ReadAll(response.Body)
+	if readError != nil {
+		log.Panic(readError)
+	}
+
+	fmt.Println(string(data))
 }
