bb7b1d8b281e7c761c5c13ba4a30be4eb9a9d480
dima <1988maksyutenko@gmail.com>
2022-09-23 15:02:23 +0300

    Added index file.

--- server/main.go
+++ server/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+)
 
 func main() {
-	fmt.Println("hello server")
+	// Retrieving current directory.
+	execFile, execError := os.Executable()
+	if execError != nil {
+		log.Panic(execError)
+	}
+	progectPath := filepath.Dir(execFile)
+
+	// Handler functions.
+	mainPage := func(w http.ResponseWriter, _ *http.Request) {
+		fileData, fileError := ioutil.ReadFile(progectPath + "/index.html")
+		if fileError != nil {
+			log.Panic(fileError)
+		}
+		w.Header().Add("Content-Type", "text/html")
+		w.Write(fileData)
+	}
+
+	http.HandleFunc("/", mainPage)
+	fmt.Println("Server is running.")
+	serverError := http.ListenAndServe(":8989", nil)
+
+	if serverError != nil {
+		log.Fatal(serverError)
+	}
 }
